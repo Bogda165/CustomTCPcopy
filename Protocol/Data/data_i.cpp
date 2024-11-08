835276bdb1f2d799526ebe29c898256e4d8923e5
Bogda165 <koval.b@icloud.com>
2024-11-08 23:08:26 +0100

    added a process for message input

---
+++ Protocol/Data/data_i.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 06/11/2024.
+//
+
+#include "data_i.h"
---
+++ Protocol/Data/data_i.h
@@ -0,0 +1,14 @@
+//
+// Created by User on 06/11/2024.
+//
+
+#ifndef PKS_PROJECT_DATA_I_H
+#define PKS_PROJECT_DATA_I_H
+
+
+class data_i {
+
+};
+
+
+#endif //PKS_PROJECT_DATA_I_H
